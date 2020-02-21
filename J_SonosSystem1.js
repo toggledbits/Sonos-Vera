62c2dd1ab27d52f7887a152b0d85922ca83ef3c0
Patrick Rigney <patrick@toggledbits.com>
2020-02-21 15:00:56 -0500

    WIP

--- J_SonosSystem1.js
+++ J_SonosSystem1.js
@@ -222,14 +222,16 @@ var SonosSystem = (function(api, $) {
 		$( '.enginesetting' ).each( function( ix, obj ) {
 			var $f = $(obj);
 			var val = $f.val() || "";
-			console.log( $f );
-			if ( ! ( isEmpty( val ) ) ){
+			console.log( $f.attr( 'id' ) );
+			if ( ! isEmpty( val ) ) {
 				var id = $f.attr( 'id' ).replace( /^val-/, "" );
 				var meta = (TTSEngines[tts.defaultengine].options || {})[id];
 				/* Write if not empty, not same as default */
 				opts[id] = val;
+				console.log("saving " + id + " = " + val);
 			}
 		});
+		console.log( opts );
 		tts.engines = tts.engines || {};
 		tts.engines[tts.defaultengine] = opts;
 		tts.version = 1;
