c4bb7549e3dbc500032d1a1274032971e5384e9a
Patrick Rigney <patrick@toggledbits.com>
2020-02-21 19:07:04 -0500

    Bump

--- J_SonosSystem1.js
+++ J_SonosSystem1.js
@@ -15,7 +15,7 @@ var SonosSystem = (function(api, $) {
 	/* unique identifier for this plugin... */
 	var uuid = '79bf9374-f989-11e9-884c-dbb32f3fa64a'; /* SonosSystem 2019-12-11 19345 */
 
-	var pluginVersion = '2.0develop-20052.1532';
+	var pluginVersion = '2.0develop-20052.1900';
 
 	var _UIVERSION = 19301;     /* must coincide with Lua core */
 
