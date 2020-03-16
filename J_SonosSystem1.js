226c1e9c7800af91a55256cc7ed55354099b4801
Patrick Rigney <patrick@toggledbits.com>
2020-03-16 10:58:22 -0400

    Bump

--- J_SonosSystem1.js
+++ J_SonosSystem1.js
@@ -15,7 +15,7 @@ var SonosSystem = (function(api, $) {
 	/* unique identifier for this plugin... */
 	var uuid = '79bf9374-f989-11e9-884c-dbb32f3fa64a'; /* SonosSystem 2019-12-11 19345 */
 
-	var pluginVersion = '2.0develop-20075.1100';
+	var pluginVersion = '2.0develop-20076.1100';
 
 	var _UIVERSION = 20073;     /* must coincide with Lua core */
 
--- L_SonosSystem1.lua
+++ L_SonosSystem1.lua
@@ -8,7 +8,7 @@
 module( "L_SonosSystem1", package.seeall )
 
 PLUGIN_NAME = "Sonos"
-PLUGIN_VERSION = "2.0develop-20075.1100"
+PLUGIN_VERSION = "2.0develop-20076.1100"
 PLUGIN_ID = 4226
 PLUGIN_URL = "https://github.com/toggledbits/Sonos-Vera"
 
