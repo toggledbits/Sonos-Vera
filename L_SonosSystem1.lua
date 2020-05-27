0fc22cef9f4b6f85ec1e76ad0c714f3bc4af7c69
Patrick Rigney <patrick@toggledbits.com>
2020-05-27 13:17:01 -0400

    Bump

--- L_SonosSystem1.lua
+++ L_SonosSystem1.lua
@@ -8,7 +8,7 @@
 module( "L_SonosSystem1", package.seeall )
 
 PLUGIN_NAME = "Sonos"
-PLUGIN_VERSION = "2.1-20147"
+PLUGIN_VERSION = "2.1-20148"
 PLUGIN_ID = 4226
 PLUGIN_URL = "https://github.com/toggledbits/Sonos-Vera"
 
